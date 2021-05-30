22795b73b8d6ea10e4a4d44d3c9d4ec9c7d68e5f
wasifuddin <79798771+wasifuddin@users.noreply.github.com>
2021-05-31 02:57:26 +0600

    Delete interne_speed_test.py

--- interne_speed_test.py
+++
@@ -1,6 +0,0 @@
-# pip install speedtest-cli
-import speedtest
-st = speedtest.Speedtest()
-dn_speed = st.download()/1000000
-up_speed = st.upload()/1000000
-print(f"Download Speed: {dn_speed} mbps Upload Speed: {up_speed} mbps")
