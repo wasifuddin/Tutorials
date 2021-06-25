835f0fd1e4717e7a7928b32f318f77e1bb9b785a
wasifuddin <79798771+wasifuddin@users.noreply.github.com>
2021-06-25 06:17:01 +0600

    Add files via upload

---
+++ msg encryption/encrypt msg.py
@@ -0,0 +1,7 @@
+# pip install stegano
+
+from stegano import lsb
+encoded_img = lsb.hide("coinpng.", "Hello World")
+encoded_img.save("encrypted_coin.png")
+decrypted_msg = lsb.reveal("encrypted_coin.png")
+print(decrypted_msg)
